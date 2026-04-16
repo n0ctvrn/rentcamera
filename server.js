0a51280c390290622569e4b2dceb52183e6e9e22
n0ctvrn <atharcroundex@gmail.com>
2026-04-16 17:28:35 +0700

    rollback

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 const auth = basicAuth({
   users: { 'admin': 'admin123' },
